0b046f0eeea79ac5412d58916c60976c64bcac86
jesse uzoma <71229278+officailesse@users.noreply.github.com>
2022-05-11 17:15:45 -0600

    added dynamic data

--- js/newmodal.js
+++ js/newmodal.js
@@ -1,80 +1,83 @@
+
+
+// dynamic starts here 
 const projects = [
-    {
+  {
 
-        id: '0',
-        name: 'Ecommece website',
-        live_version: 'https://github.com/HeDevedUp/MicroversePortfolioProject',
-        description: 'A portfolio website .  ',
-        featured_image: 'assert/Rectangle21.png',
-        arrow_img: "assert/ic_arrow_right.png",
-        source: 'https://github.com/HeDevedUp/MicroversePortfolioProject',
-        technologies: ['Git', 'CSS', 'HTML', 'JavaScript']
-    },
-    {
-        id: '1',
-        name: 'Portfolio project',
-        live_version: 'https://github.com/HeDevedUp/MicroversePortfolioProject',
-        description: 'A portfolio website .  ',
-        featured_image: 'assert/Rectangle212.png',
-        arrow_img: "assert/ic_arrow_right.png",
-        source: 'https://github.com/HeDevedUp/MicroversePortfolioProject',
-        technologies: ['Git', 'CSS', 'HTML', 'JavaScript']
-    },
-    {
-        id: '2',
-        name: 'Shopping Site',
-        live_version: 'https://github.com/HeDevedUp/MicroversePortfolioProject',
-        description: 'A portfolio website .  ',
-        featured_image: 'assert/Rectangle213.png',
-        arrow_img: "assert/ic_arrow_right.png",
-        source: 'https://github.com/HeDevedUp/MicroversePortfolioProject',
-        technologies: ['JavaScript', 'Git', 'CSS', 'HTML',]
-    },
-    {
-        id: '3',
-        name: 'Project name goes here',
-        live_version: 'https://github.com/HeDevedUp/MicroversePortfolioProject',
-        description: 'A portfolio website .  ',
-        featured_image: 'assert/Rectangle214.png',
-        arrow_img: "assert/ic_arrow_right.png",
-        source: 'https://github.com/HeDevedUp/MicroversePortfolioProject',
-        technologies: ['Git', 'HTML', 'CSS', 'JavaScript']
+    id: '0',
+    name: 'Ecommece website',
+    live_version: 'https://github.com/HeDevedUp/MicroversePortfolioProject',
+    description: 'A portfolio website .  ',
+    featured_image: 'assert/Rectangle21.png',
+    arrow_img: "assert/ic_arrow_right.png",
+    source: 'https://github.com/HeDevedUp/MicroversePortfolioProject',
+    technologies: ['Git', 'CSS', 'HTML', 'JavaScript']
+  },
+  {
+    id: '1',
+    name: 'Portfolio project',
+    live_version: 'https://github.com/HeDevedUp/MicroversePortfolioProject',
+    description: 'A portfolio website .  ',
+    featured_image: 'assert/Rectangle212.png',
+    arrow_img: "assert/ic_arrow_right.png",
+    source: 'https://github.com/HeDevedUp/MicroversePortfolioProject',
+    technologies: ['Git', 'CSS', 'HTML', 'JavaScript']
+  },
+  {
+    id: '2',
+    name: 'Shopping Site',
+    live_version: 'https://github.com/HeDevedUp/MicroversePortfolioProject',
+    description: 'A portfolio website .  ',
+    featured_image: 'assert/Rectangle213.png',
+    arrow_img: "assert/ic_arrow_right.png",
+    source: 'https://github.com/HeDevedUp/MicroversePortfolioProject',
+    technologies: ['JavaScript', 'Git', 'CSS', 'HTML',]
+  },
+  {
+    id: '3',
+    name: 'Project name goes here',
+    live_version: 'https://github.com/HeDevedUp/MicroversePortfolioProject',
+    description: 'A portfolio website .  ',
+    featured_image: 'assert/Rectangle214.png',
+    arrow_img: "assert/ic_arrow_right.png",
+    source: 'https://github.com/HeDevedUp/MicroversePortfolioProject',
+    technologies: ['Git', 'HTML', 'CSS', 'JavaScript']
 
-    },
-    {
-        id: '3',
-        name: 'Project name goes here',
-        live_version: 'https://github.com/HeDevedUp/MicroversePortfolioProject',
-        description: 'A portfolio website .  ',
-        featured_image: 'assert/Rectangle215.png',
-        arrow_img: "assert/ic_arrow_right.png",
-        source: 'https://github.com/HeDevedUp/MicroversePortfolioProject',
-        technologies: ['CSS', 'Git', 'HTML', 'JavaScript']
-    },
-    {
-        id: '4',
-        name: 'Project name goes here',
-        live_version: 'https://github.com/HeDevedUp/MicroversePortfolioProject',
-        description: 'A portfolio website .  ',
-        featured_image: 'assert/Rectangle211.png',
-        arrow_img: "assert/ic_arrow_right.png",
-        source: 'https://github.com/HeDevedUp/MicroversePortfolioProject',
-        technologies: ['Git', 'CSS', 'HTML', 'JavaScript']
-    }
+  },
+  {
+    id: '3',
+    name: 'Project name goes here',
+    live_version: 'https://github.com/HeDevedUp/MicroversePortfolioProject',
+    description: 'A portfolio website .  ',
+    featured_image: 'assert/Rectangle215.png',
+    arrow_img: "assert/ic_arrow_right.png",
+    source: 'https://github.com/HeDevedUp/MicroversePortfolioProject',
+    technologies: ['CSS', 'Git', 'HTML', 'JavaScript']
+  },
+  {
+    id: '4',
+    name: 'Project name goes here',
+    live_version: 'https://github.com/HeDevedUp/MicroversePortfolioProject',
+    description: 'A portfolio website .  ',
+    featured_image: 'assert/Rectangle211.png',
+    arrow_img: "assert/ic_arrow_right.png",
+    source: 'https://github.com/HeDevedUp/MicroversePortfolioProject',
+    technologies: ['Git', 'CSS', 'HTML', 'JavaScript']
+  }
 ]
 
-const dynamic = document.querySelector('.section2');
-for (let a = 0; a < projects.length; a++) {
-    console.log(projects[a].name)
-    const fetch = document.querySelector('.section2').innerHTML;
+const dynamicData = document.querySelector('.section2');
+for (let a = 0; a < projects.length; a += 1) {
+  console.log(projects[a].name)
+  const getClass = document.querySelector('.section2').innerHTML;
 
-    const imageNum = a % 2 === 0 ? 'fourth_img' : 'third_img';
-    const cardNum = a % 2 === 0 ? 'fourth__card' : 'third__card';
+  const imageindex = a % 2 === 0 ? '' : 'third_img';
+  const cardindex = a % 2 === 0 ? 'fourth__card' : 'third__card';;
 
-    dynamic.innerHTML = ` 
-    <div class="project-container" ${cardNum}>
+  dynamicData.innerHTML = ` 
+    <div class="project-container" ${cardindex}>
     <img
-    ${imageNum}
+    ${imageindex}
      class="project-image" 
      loading="lazy"
     alt="${projects[a].name}" 
@@ -87,7 +90,7 @@ for (let a = 0; a < projects.length; a++) {
             <li>Ruby on Rails</li>
             <li>Javascript</li>
         </ul>
-        <button type="button" class="btn button-with-arrow">
+        <button ${projects[a].id} type="button" class="btn button-with-arrow"  data-modal-target="#modal">
             See this project
             <img
             loading="lazy"
@@ -95,22 +98,79 @@ for (let a = 0; a < projects.length; a++) {
         </button>
     </div>
     </div>
-  ${fetch}`
+  ${getClass}`
 }
+/// ends here 
 
+const openModalButtons = document.querySelectorAll('[data-modal-target]');
+const closeModalButtons = document.querySelectorAll('[data-close-button]');
+const overlayy = document.getElementById('overlayy');
+const modalContent = document.querySelector('.modalData');
 
+const openModal = (modal) => {
+  if (modal == null) return;
+  modal.classList.add('active');
+  overlayy.classList.add('active');
+};
 
+const closeModal = (modal) => {
+  if (modal == null) return;
+  modal.classList.remove('active');
+  overlayy.classList.remove('active');
+};
 
+openModalButtons.forEach((button) => {
+  button.addEventListener('click', (e) => {
+    const foundIndex = Array.from(openModalButtons).indexOf(e.target);
+    modalContent.innerHTML = `
+      <div class="modal-body">
+        <img
+          class="modal-img"
+          src=" ${projects[foundIndex].featured_image}"
+          alt="${projects[foundIndex].name}"
+          title="${projects[foundIndex].name}"
+        />
+        <div class="modal-text">
+          <p>
+            ${projects[foundIndex].description}
+          </p>
+        </div>
 
+        <div class="projects__languages">
+          <ul class="projects__languages--list">
+                               <li class='projects__languages--first'>HTML </li>
+                    <li class='projects__languages--first'>CSS </li>
+                    <li class='projects__languages--first'>JavaScript </li>
+                    <li class='projects__languages--first'>Rails </li>
+          </ul>
+        </div>
+        <div class="btn-container">
+          <a href="${projects[foundIndex].live_version}" type="button" class="modal-btn">
+            See Live Link <img src="/images/Icon.png" alt="Icon" title="Img" class="modal-icon">
+          </a>
+          <a href="${projects[foundIndex].source}" type="button" class="modal-btn">
+            See Source Code
+            <i class="fa fa-github modal-icon" aria-hidden="true"></i>
+          </a>
+        </div>
+      </div>`;
+    openModal(modal);
+  });
+});
 
+overlayy.addEventListener('click', () => {
+  const modals = document.querySelectorAll('.modal.active');
+  modals.forEach((modal) => {
+    closeModal(modal);
+  });
+});
 
-
-
-
-
-
-
-
+closeModalButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    const modal = button.closest('.modal');
+    closeModal(modal);
+  });
+});
 
 
 
--- js/project.js
+++ js/project.js
@@ -1,44 +1,46 @@
-const projects = [
-    {
-        id: '0',
-        name: 'Christ Model Academy',
-        live_version: 'http://christmodel.academy',
-        description: 'A School Website build for Christ model Academy.  ',
-        featured_image: 'images/projects/3.png',
-        source: 'https://github.com/learnwithalfred',
-        technologies: ['Laravel', 'PHP', 'HTML', 'JavaScript'],
-    },
-    {
-        id: '1',
-        name: 'Aura Geospatial Website',
-        description:
-            'A website build for a Drones Spraying Company. I build this website on freelance.  ',
-        featured_image: 'images/projects/1.png',
-        live_version: 'https://aurageospatial.com',
-        source: 'https://github.com/learnwithalfred',
-        technologies: ['HTML', 'React', 'JavaScript'],
-    },
 
-    {
-        id: '2',
-        name: 'babylontruckingllc website',
-        featured_image: 'images/projects/2.png',
-        description: 'A freelance project built for babylon logistics company. ',
-        live_version: 'https://www.babylontruckingllc.com',
-        source: 'https://github.com/learnwithalfred',
-        technologies: ['HTML', 'React', 'JavaScript'],
-    },
+/// dynamic starts here 
+const projects = [
+  {
+    id: '0',
+    name: 'Christ Model Academy',
+    live_version: 'http://christmodel.academy',
+    description: 'A School Website build for Christ model Academy.  ',
+    featured_image: 'images/projects/3.png',
+    source: 'https://github.com/learnwithalfred',
+    technologies: ['Laravel', 'PHP', 'HTML', 'JavaScript'],
+  },
+  {
+    id: '1',
+    name: 'Aura Geospatial Website',
+    description:
+      'A website build for a Drones Spraying Company. I build this website on freelance.  ',
+    featured_image: 'images/projects/1.png',
+    live_version: 'https://aurageospatial.com',
+    source: 'https://github.com/learnwithalfred',
+    technologies: ['HTML', 'React', 'JavaScript'],
+  },
+
+  {
+    id: '2',
+    name: 'babylontruckingllc website',
+    featured_image: 'images/projects/2.png',
+    description: 'A freelance project built for babylon logistics company. ',
+    live_version: 'https://www.babylontruckingllc.com',
+    source: 'https://github.com/learnwithalfred',
+    technologies: ['HTML', 'React', 'JavaScript'],
+  },
 ];
 
 const dynamic = document.querySelector('.card-container');
 
 for (let i = 0; i < projects.length; i += 1) {
-    const fetch = document.querySelector('.card-container').innerHTML;
+  const fetch = document.querySelector('.card-container').innerHTML;
 
-    const imageNum = i % 2 === 0 ? 'fourth_img' : 'third_img';
-    const cardNum = i % 2 === 0 ? 'fourth__card' : 'third__card';
+  // const imageNum = i % 2 === 0 ? '' : 'third_img';
+  // const cardNum = i % 2 === 0 ? 'fourth__card' : 'third__card';
 
-    dynamic.innerHTML = ` 
+  dynamic.innerHTML = ` 
       <div class="card">
               <div class="project_container ${cardNum}">
                 <div class="card__image ${imageNum}">
@@ -66,7 +68,7 @@ for (let i = 0; i < projects.length; i += 1) {
                     </ul>
                   </div>
                   <div class="projects__btn">
-                    <button id='${projects[i].id}' type="button" class="btn" data-modal-target="#modal">See Projects</button>
+                    <button id='${projects[i].id}' type="button" class="btn button-with-arrow" data-modal-target="#modal>See Projects</button>
                   </div>
                 </div>
               </div>
@@ -80,21 +82,21 @@ const overlay = document.getElementById('overlay');
 const modalContent = document.querySelector('.modalData');
 
 const openModal = (modal) => {
-    if (modal == null) return;
-    modal.classList.add('active');
-    overlay.classList.add('active');
+  if (modal == null) return;
+  modal.classList.add('active');
+  overlay.classList.add('active');
 };
 
 const closeModal = (modal) => {
-    if (modal == null) return;
-    modal.classList.remove('active');
-    overlay.classList.remove('active');
+  if (modal == null) return;
+  modal.classList.remove('active');
+  overlay.classList.remove('active');
 };
 
 openModalButtons.forEach((button) => {
-    button.addEventListener('click', (e) => {
-        const foundIndex = Array.from(openModalButtons).indexOf(e.target);
-        modalContent.innerHTML = `
+  button.addEventListener('click', (e) => {
+    const foundIndex = Array.from(openModalButtons).indexOf(e.target);
+    modalContent.innerHTML = `
         <div class="modal-body">
           <img
             class="modal-img"
@@ -126,21 +128,21 @@ openModalButtons.forEach((button) => {
             </a>
           </div>
         </div>`;
-        const modal = document.querySelector(button.dataset.modalTarget);
-        openModal(modal);
-    });
+    // const modal = document.querySelector(button.dataset.modalTarget);
+    openModal(modal);
+  });
 });
 
 overlay.addEventListener('click', () => {
-    const modals = document.querySelectorAll('.modal.active');
-    modals.forEach((modal) => {
-        closeModal(modal);
-    });
+  const modals = document.querySelectorAll('.modal.active');
+  modals.forEach((modal) => {
+    closeModal(modal);
+  });
 });
 
 closeModalButtons.forEach((button) => {
-    button.addEventListener('click', () => {
-        const modal = button.closest('.modal');
-        closeModal(modal);
-    });
+  button.addEventListener('click', () => {
+    const modal = button.closest('.modal');
+    closeModal(modal);
+  });
 });
