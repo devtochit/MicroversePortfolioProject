2b795d1d08aa81be0c8a037c85dc666b6b68cd16
officialesse <uzomaesse@gmail.com>
2022-05-13 14:09:32 +0100

    making changes to contact validation

--- js/contact-form.js
+++ js/contact-form.js
@@ -1,125 +1,121 @@
 const form = document.querySelector('.formidable');
-const fullName = document.querySelector('#username');
-const email = document.querySelector('#email');
-const message = document.querySelector('#message');
-const error = document.querySelector('#error-message');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const textBox = document.getElementById('message');
+const inputArr = [ username,email,textBox ]
 
-const isRequired = (value) => value !== '';
-const isBetween = (length, min, max) => !(length < min || length > max);
 
-const isEmailValid = (email) => {
+
+const isEmailValid = function(input){
     const re = /^(([^<>()\\.,;:\s@"]+(\.[^<>()\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-z\-0-9]+\.)+[a-z]{2,}))$/;
-    return re.test(email);
+          if (re.test(String(input.value).toLocaleLowerCase().trim())){
+              showSuccess( input)
+          }else{
+              showError ( input, 'Email is not valid ')
+          }
 };
 
-const showError = (input, message) => {
-    const formField = input.parentElement;
-    formField.classList.remove('success');
-    formField.classList.add('error');
-    const error = formField.querySelector('small');
-    error.value = message;
-};
+    const message = function(input) {
+    const errorMessage = input.name.replace(/-p/, ' P');
+    return errorMessage.charAt(0).toUpperCase() + errorMessage.slice(1);
+}
 
-const showSuccess = (input) => {
-    const formField = input.parentElement;
-    formField.classList.remove('error');
-    formField.classList.add('success');
-    const error = formField.querySelector('small');
-    error.value = '';
-};
+ const  showError =(input, message )=>{
+     let formControl = input.parentElement;
+     formControl.classList=' form-container error';
+     const small = formControl.querySelector('small')
+     small.innerText = message;
+ }
+ const showSuccess=( input )=>{
+  let formControl=input.parentElement;
+ formControl.classList =' form-container success'
+ }
 
-const checkFullName = () => {
-    let valid = false;
-    const min = 3;
-    const max = 25;
-    const username = fullName.value.trim();
 
-    if (!isRequired(username)) {
-        showError(fullName, 'Name cannot be blank.');
-    } else if (!isBetween(username.length, min, max)) {
-        showError(fullName, `Name must be between ${min} and ${max} characters.`);
-    } else {
-        showSuccess(fullName);
-        valid = true;
+ const checkRequired= function(inputArr){ 
+    inputArr.forEach( input =>{
+  if( input.value.trim()===''){
+     showError(input, `${ message(input)} is required.`);
+     return
+  } else {
+      showSuccess(input )
+  }
+    })
+
+ }
+
+
+ const checkLength = function(input, min, max) {
+    if (input.value.trim().length < min) {
+        showError(input, `${message(input)} must contain ${min} characters`);
+    } else if (input.value.trim().length > max) {
+        showError(input, `${message(input)} must be less than ${max} characters`);
+    } else{
+        showSuccess(input);
     }
-    return valid;
-};
+}
 
-const checkEmail = () => {
-    let valid = false;
-    const emailVal = email.value.trim();
-    if (!isRequired(emailVal)) {
-        showError(email, 'Email cannot be blank.');
-        error.innerHTML = 'Email cannot be blank';
-    } else if (!isEmailValid(emailVal)) {
-        showError(email, 'Email is not valid.');
-        error.innerHTML = 'Email is not valid';
-    } else if (email.value !== email.value.toLowerCase()) {
-        showError(email, 'Email must be in lowercase');
-        error.innerHTML = 'Email must be in lowercase';
-    } else {
-        showSuccess(email);
-        valid = true;
-    }
-    return valid;
-};
 
-const checkMessage = () => {
-    let valid = false;
-    const min = 10;
-    const max = 500;
-    const msgVal = message.value.trim();
 
-    if (!isRequired(msgVal)) {
-        showError(message, 'Message cannot be blank.');
-    } else if (!isBetween(msgVal.length, min, max)) {
-        showError(message, `Message must be between ${min} and ${max} characters.`);
-    } else {
-        showSuccess(message);
-        valid = true;
-    }
-    return valid;
-};
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const isUsernameValid = checkFullName();
-    const isEmailValid = checkEmail();
-    const isMessageValid = checkMessage();
-    const isFormValid = isUsernameValid && isEmailValid && isMessageValid;
+ form.addEventListener('submit',(e)=>{
+e.preventDefault();
+checkRequired(inputArr)
+isEmailValid (email) 
 
-    if (isFormValid) {
-        form.submit();
-    }
-});
+ })
 
-const debounce = (fn, delay = 500) => {
-    let timeoutId;
-    return (...args) => {
-        if (timeoutId) {
-            clearTimeout(timeoutId);
-        }
-        timeoutId = setTimeout(() => {
-            fn(...args);
-        }, delay);
-    };
-};
 
-form.addEventListener(
-    'input',
-    debounce((e) => {
-        switch (e.target.id) {
-            case 'name':
-                checkFullName();
-                break;
-            case 'email':
-                checkEmail();
-                break;
-            case 'message':
-                checkMessage();
-                break;
-            default:
-                break;
-        }
-    }),
-);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
