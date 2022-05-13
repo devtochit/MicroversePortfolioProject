f70f67c6555c9f37633fba3e0d489cfbf9133c19
jesse uzoma <71229278+officailesse@users.noreply.github.com>
2022-05-13 16:07:22 -0600

    completed local storage

---
+++ js/storage.js
@@ -0,0 +1,30 @@
+const form = document.querySelector('.formidable');
+const nameField = formStorage.elements.name;
+const emailField = formStorage.elements.email;
+const messageField = formStorage.elements.message;
+
+function dataStorage() {
+    const userData = {
+        name: nameField.value,
+        emailAddress: emailField.value,
+        message: messageField.value,
+    };
+    localStorage.setItem('formData', JSON.stringify(userData));
+}
+
+function inputValues() {
+    const formData = JSON.parse(localStorage.getItem('formData'));
+    if (formData !== null) {
+        nameField.value = formData.name;
+        emailField.value = formData.emailAddress;
+        messageField.value = formData.message;
+    }
+}
+
+window.addEventListener('load', inputValues);
+
+for (let n = 0; n < formStorage.length; n += 1) {
+    formStorage[n].addEventListener('change', () => {
+        dataStorage();
+    });
+}
